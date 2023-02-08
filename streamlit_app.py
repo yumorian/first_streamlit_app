1e443993af8a1246f54dcd2e57daad8c15f331dd
yumorian <61497573+yumorian@users.noreply.github.com>
2023-02-08 15:16:18 -0800

    Update streamlit_app.py
    take a json format to normalized version. and make it to the table format.

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,4 +26,9 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json()) # just writes the data to the screen
+
+# take a json format and convert it to normalize version. put it in the variance.
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# output it as a table by using dataframe function
+streamlit.dataframe(fruityvice_normalized)
