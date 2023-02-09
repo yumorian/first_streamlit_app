6b0d3eb241ac1fcbc925b46e019868b7ee4dd43f
yumorian <61497573+yumorian@users.noreply.github.com>
2023-02-08 17:19:30 -0800

    Update streamlit_app.py
    added : it was a syntax error

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ except URLError as e:
 streamlit.header("The fruit load list contains:")
 # snowflake relates function
 def get_fruit_load_list():
-    with my_cnx.cursor() as my_cur
+    with my_cnx.cursor() as my_cur:
     my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
     return my_cur.fetchall()
 
