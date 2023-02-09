9c7acca77ecaf95c924d6a1a7c303a8c0f7ef937
yumorian <61497573+yumorian@users.noreply.github.com>
2023-02-08 17:01:02 -0800

    Update streamlit_app.py
    stopped the code in the middle, and brought all the libraries import to the top

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -10,7 +14,6 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -26,7 +29,6 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # take a json format and convert it to normalize version. put it in the variance.
@@ -34,7 +36,9 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output it as a table by using dataframe function
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
+#don't run anything below here!
+streamlit.stop()
+
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
